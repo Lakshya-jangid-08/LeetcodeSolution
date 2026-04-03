1af0087c4e5869c933692326d4cd631f33a0a44f
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-04-04 00:57:46 +0530

    File updated at 4/4/2026, 12:57:45 am

--- 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.cpp
+++ 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.cpp
@@ -1,26 +1,34 @@
-// Last updated: 4/4/2026, 12:55:26 am
-1class Solution {
-2public:
-3    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-4        priority_queue< pair<double, int> > pq;
-5        int id = 0;
-6        for(auto cord : points) {
-7            int x = cord[0], y = cord[1];
-8            double dis = sqrt(1LL * x*x + 1LL * y*y);
-9            pq.push({dis, id});
-10        
-11            if(pq.size() > k) {
-12                pq.pop();
-13            }
-14            id++;
-15        }
-16        vector<vector<int>> res;
-17        while(!pq.empty()) {
-18            int idx = pq.top().second;
-19            res.push_back(points[idx]);
-20            pq.pop();
-21        }
-22
-23        return res;
-24    }
-25};
+// Last updated: 4/4/2026, 12:57:45 am
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        int l = 0, r = points.size() - 1;
+        --K;
+
+        while(true) {
+            int p = partition(points, l, r);
+            if (p == K) break;
+
+            if (p < K) l = p + 1;
+            else r = p - 1;
+        }
+
+        return vector<vector<int>>(points.begin(), points.begin() + K + 1);
+    }
+
+private:
+    int distance(vector<int>& p) {
+        return p[0]*p[0] + p[1]*p[1];
+    }
+    
+    int partition(vector<vector<int>>& arr, int low, int high) {
+        int pivot = distance(arr[high]);
+        for (int i = low; i < high; ++i) {
+            if(distance(arr[i]) < pivot)
+                swap(arr[i], arr[low++]);
+        }
+
+        swap(arr[high], arr[low]);
+        return low;
+    }
+};
