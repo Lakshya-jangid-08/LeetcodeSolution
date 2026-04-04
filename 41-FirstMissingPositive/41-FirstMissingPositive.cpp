2da8dc8b9c9b17b59d175a1d731b1f270f9b4a71
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-04-04 23:13:30 +0530

    File updated at 4/4/2026, 11:13:28 pm

--- 41-FirstMissingPositive/41-FirstMissingPositive.cpp
+++ 41-FirstMissingPositive/41-FirstMissingPositive.cpp
@@ -1,30 +1,35 @@
-// Last updated: 4/4/2026, 11:11:32 pm
-1class Solution {
-2public:
-3    int firstMissingPositive(vector<int>& nums) {
-4        bool contains_ones = false;   
-5        for(auto &x : nums) {
-6            if(x == 1) {
-7                contains_ones = true;
-8            }
-9            if(x <= 0 || x > nums.size()) x = 1;
-10        }
-11        if(!contains_ones) return 1;
-12
-13        for(int i = 0; i < nums.size(); i++) {
-14            // number present hai ? 
-15            int val = abs(nums[i]);
-16            // number 1 to N hai toh joh number present hai
-17            // uska nums[val - 1] neg kr deta hu 
-18            // mtlb present haii 
-19            // Agar > 0 nhi hai mtlb woh idx kbhi process nhi huwa
-20            // mtlb woh missing hai
-21            nums[val - 1] = abs(nums[val - 1]) * -1; 
-22        }
-23
-24        for(int i = 0; i < nums.size(); i++) {
-25            if(nums[i] > 0) return i + 1;
-26        }
-27        return nums.size() + 1;
-28    }
-29};
+// Last updated: 4/4/2026, 11:13:28 pm
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int count=0;
+
+       // for(auto i: nums) if(i>0) count++;
+
+        // missing number can be the range of 1 to count+1 inclusive
+        //count++;
+        // int sum = count*(count+1) /2;
+        //int sum2=0;
+        //for(auto i:nums) if(i<=count && i>0) sum2+=i;
+
+        // return sum-sum2;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            while(nums[i] > 0 && nums[i] <= nums.size() && nums[nums[i]-1] != nums[i])
+            {
+                swap(nums[i], nums[nums[i]-1]);
+            }
+        }
+        //for(auto i: nums) cout<<i<<" ";
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i+1!=nums[i]) return i+1;
+        }
+
+
+
+
+        return nums.size()+1;
+    }
+};
