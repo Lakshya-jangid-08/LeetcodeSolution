ace388a9dac1afabd4d8156184fe38238a76e7e9
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-04-04 10:29:00 +0530

    File updated at 4/4/2026, 10:29:00 am

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,19 +1,46 @@
-// Last updated: 4/4/2026, 10:25:38 am
-1class Solution {
-2public:
-3    int lengthOfLongestSubstring(string s) {
-4        unordered_map<char, int> mp;
-5
-6        int l = 0;
-7        int len = 0;
-8        for(int r = 0; r < s.length(); r++) {
-9            mp[s[r]]++;
-10            while(mp[s[r]] > 1) {
-11                mp[s[l]]--;
-12                l++;
-13            }
-14            len = max(len, r - l + 1);
-15        }
-16        return len;
-17    }
-18};
+// Last updated: 4/4/2026, 10:29:00 am
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+        
+//         if(s.size()==0)return 0;
+//         int ans=0, count=0;
+//         int left=0, right=0;
+//         int freq[128]={0};
+
+//         while(right<s.size())
+//         {
+//             if(freq[s[right]]==1)
+//             {
+//                 freq[s[left]]--;
+//                 left++;
+//                 count--;
+//             }
+//             else
+//             {
+//                 freq[s[right]]++;
+//                 right++;
+//                 count++;
+//             } 
+//             ans = max(count,ans);
+//         }
+
+//         return ans;
+//     }
+// };
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> last(128, -1); // last seen index
+        int l = 0, ans = 0;
+
+        for (int r = 0; r < s.size(); r++) {
+            if (last[s[r]] >= l) {
+                l = last[s[r]] + 1; // shrink window
+            }
+            last[s[r]] = r;
+            ans = max(ans, r - l + 1);
+        }
+        return ans;
+    }
+};
