5d72c68cf9d366d2f5971d38d6af9872d8195816
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-04-06 10:42:20 +0530

    File updated at 6/4/2026, 10:42:20 am

--- 542-01Matrix/542-01Matrix.cpp
+++ 542-01Matrix/542-01Matrix.cpp
@@ -1,39 +1,33 @@
-// Last updated: 6/4/2026, 10:41:26 am
-1class Solution {
-2public:
-3    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-4        queue<pair<int, int>> q;
-5
-6        vector<vector<int>> res(mat.size(), vector<int>(mat[0].size(), -1)); 
-7
-8        for(int i = 0; i < mat.size(); i++) {
-9            for(int j = 0; j < mat[0].size(); j++) {
-10                if(mat[i][j]) continue;
-11                q.push({i, j});
-12                res[i][j] = 0;
-13            }
-14        }
-15        int dx[] = {-1, 1, 0, 0};
-16        int dy[] = {0, 0, -1, 1};
-17        int dist = 0;
-18        while(!q.empty()) {
-19            int lvl = q.size();
-20            while(lvl--) {
-21                auto [x, y] = q.front(); q.pop();
-22                for(int i = 0; i < 4; i++) {
-23                    int u = x + dx[i];
-24                    int v = y + dy[i];
-25
-26                    if(u < 0 || v < 0) continue;
-27                    if(u >= mat.size() || v >= mat[0].size()) continue;
-28                    if(res[u][v] != -1) continue;
-29                    res[u][v] = dist + 1;
-30                    q.push({ u, v});
-31                }
-32            }
-33            dist++;
-34        }
-35        return res;
-36
-37    }
-38};
+// Last updated: 6/4/2026, 10:42:20 am
+/*
+what if there is no zeros then ? 
+
+*/
+
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        vector<int> d = { -1 , 0 , 1 , 0 , -1 };
+        int m = mat.size() , n = mat[0].size();
+        queue<pair<int,int>> q;
+        for( int i = 0 ; i < m ; i++ ){
+            for( int j = 0 ; j < n ; j++ ){
+                if( mat[i][j] == 0 ) q.push({i,j});
+                else mat[i][j] = -1;
+            }
+        }
+
+        while( !q.empty() ){
+            auto [x,y] = q.front(); q.pop();
+            for( int i = 0 ; i < 4 ; i++ ){
+                int mx = x + d[i] , ny = y + d[i+1];
+                if( mx < 0 || mx == m || ny < 0 || ny == n || mat[mx][ny] != -1 ) continue;
+                mat[mx][ny] = mat[x][y] + 1;
+                q.push({mx,ny});
+            }
+        }
+
+        return mat;
+        
+    }
+};
