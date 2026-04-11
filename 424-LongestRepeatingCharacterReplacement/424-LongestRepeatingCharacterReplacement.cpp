f98cad9856f0562ce323b2bcf7310ea3276eef1a
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-04-11 13:55:59 +0530

    File updated at 11/4/2026, 1:55:59 pm

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
@@ -1,22 +1,23 @@
-// Last updated: 11/4/2026, 12:43:11 am
-class Solution {
-public:
-    int characterReplacement(string s, int k) {
-        vector<int> freq(26, 0);
-        int l = 0, maxFreq = 0, maxLen = 0;
-
-        for (int r = 0; r < s.size(); r++) {
-            freq[s[r] - 'A']++;
-            maxFreq = max(maxFreq, freq[s[r] - 'A']);
-
-            while ((r - l + 1) - maxFreq > k) {
-                freq[s[l] - 'A']--;
-                l++;
-            }
-
-            maxLen = max(maxLen, r - l + 1);
-        }
-
-        return maxLen;
-    }
-};
+// Last updated: 11/4/2026, 1:55:59 pm
+1class Solution {
+2public:
+3    int characterReplacement(string s, int k) {
+4        int freq[26] = {0};
+5        
+6        int l = 0, maxFreq = 0, maxLen = 0;
+7
+8        for (int r = 0; r < s.size(); r++) {
+9            freq[s[r] - 'A']++;
+10            maxFreq = max(maxFreq, freq[s[r] - 'A']);
+11
+12            if(r - l + 1 - maxFreq > k) {
+13                freq[s[l] - 'A']--;
+14                l++;
+15            }
+16
+17            maxLen = max(maxLen, r - l + 1);
+18        }
+19
+20        return maxLen;
+21    }
+22};
