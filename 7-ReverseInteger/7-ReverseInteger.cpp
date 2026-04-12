6c2cfac7a1048c24000d581e5307c4574d79d7ce
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-04-12 12:54:54 +0530

    File updated at 12/4/2026, 12:54:53 pm

--- 7-ReverseInteger/7-ReverseInteger.cpp
+++ 7-ReverseInteger/7-ReverseInteger.cpp
@@ -1,19 +1,13 @@
-// Last updated: 12/4/2026, 12:54:07 pm
-1class Solution {
-2public:
-3    int reverse(int temp) {
-4        long long res = 0;
-5        long long mult = 1;
-6        long long x = temp;
-7        if(x < 0) {
-8            mult = -1;
-9            x = - 1LL * temp;
-10        }
-11        while(x > 0) {
-12            res = res * 10 + (x % 10);
-13            if(res > INT_MAX || res < INT_MIN) return 0;
-14            x /= 10;
-15        }
-16        return res * mult;
-17    }
-18};
+// Last updated: 12/4/2026, 12:54:53 pm
+class Solution {
+public:
+    int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            if (result > INT_MAX / 10 || result < INT_MIN / 10) return 0;
+            result = result * 10 + x % 10;
+            x /= 10;
+        }
+        return result;
+    }
+};
