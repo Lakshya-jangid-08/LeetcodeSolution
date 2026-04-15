0eda506a3c31c6065459ebeeaa7b9e1954fbc7af
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-04-15 22:14:24 +0530

    File updated at 4/15/2026, 10:14:22 PM

--- 1248-CountNumberofNiceSubarrays/1248-CountNumberofNiceSubarrays.cpp
+++ 1248-CountNumberofNiceSubarrays/1248-CountNumberofNiceSubarrays.cpp
@@ -1,25 +1,23 @@
-// Last updated: 4/15/2026, 10:09:15 PM
-1class Solution {
-2public:
-3    int numberOfSubarrays(vector<int>& nums, int k) {
-4        int n = nums.size();
-5        unordered_map<int, int> prefix;
-6        int cnt = 0;
-7        int res = 0;
-8        prefix[0] = 1;
-9        for(int i = 0; i < n; i++) {
-10            if(nums[i] % 2 == 1)
-11                cnt++;
-12
-13            int target = cnt - k;
-14            if(prefix.count(target)) {
-15                res += prefix[target];
-16            } 
-17            
-18            prefix[cnt]++;
-19        }
-20
-21        return res;
-22
-23    }
-24};
+// Last updated: 4/15/2026, 10:14:22 PM
+class Solution {
+public:
+int helper(vector<int>& nums, int k){
+    if(k<0) return 0;
+    int count=0;
+    int sub=0;
+    int l=0;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i] & 1) count++;
+        while(count>k){
+            if(nums[l] & 1) count--;
+            l++;
+        }
+        sub+=(i-l+1);
+    }
+    return sub;
+}
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        // cout<<helper(nums,k)<<" "<<helper(nums,k-1);
+        return (helper(nums,k)-helper(nums,k-1));
+    }
+};
