4515e32785f2ba5466616e316fe8f574bc2d6ad1
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-04-15 10:24:20 +0530

    File updated at 4/15/2026, 10:24:19 AM

--- 480-SlidingWindowMedian/480-SlidingWindowMedian.cpp
+++ 480-SlidingWindowMedian/480-SlidingWindowMedian.cpp
@@ -1,66 +1,57 @@
-// Last updated: 12/4/2026, 10:30:04 pm
+// Last updated: 4/15/2026, 10:24:19 AM
 1class Solution {
 2public:
-3    vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-4        vector<double> res;
-5        // store first half [0 ... n/2]
-6        set<pair<int, int>, greater<pair<int,int>> > maxset; 
-7        
-8        // store second half [n/2 .... n]
-9        set<pair<int, int>> minset;
-10
-11        for(int i = 0; i < k; i++) {
-12            minset.insert({nums[i], i});
-13        }
-14
-15        for(int i = 0; i < k/2; i++) {
-16            auto it = minset.begin(); 
-17            maxset.insert(*it);
-18            minset.erase(it);
-19        }
-20        double median = 1.0;
-21        if(k % 2)
-22            median = minset.begin()->first;
-23        else
-24            median = (1.0 * maxset.begin()->first + 1.0 * minset.begin()->first) / 2.0;
-25        
-26        res.push_back(median);
-27        int r = k, l = 0;
-28
-29        while(r < nums.size()) {
-30            
-31            pair<int,int> add = {nums[r], r};
-32            pair<int,int> remove = {nums[l], l};
-33            int f = 1;
-34            if(maxset.find(remove) != maxset.end()) {
-35                maxset.erase(remove);
-36                f--;
-37            } else {
-38                minset.erase(remove);
-39            }
-40
-41            if(f) {
-42                maxset.insert(add);
-43                auto it = maxset.begin();
-44                minset.insert(*it);
-45                maxset.erase(it);
-46            } else {
-47                minset.insert(add);
-48                auto it = minset.begin();
-49                maxset.insert(*it);
-50                minset.erase(it);
-51            }
-52
-53
-54            if(k % 2)
-55                median = minset.begin()->first;
-56            else
-57                median = (1.0 * maxset.begin()->first + 1.0 * minset.begin()->first) / 2.0;
-58            
-59            res.push_back(median);
-60            r++; l++;
-61        }
-62
-63        return res;
-64    }
-65};
+3int maxEqualChocolates(vector<int>& nums, int k) {
+4    sort(nums.begin(), nums.end());
+5    int n = nums.size();
+6    int maxElem = nums[n - 1];
+7
+8    vector<long long> diff(n, 0);
+9
+10    for(int i = 0; i < n; i++) {
+11        diff[i] = maxElem - nums[i];
+12    }
+13
+14    vector<long long>prefix(n, 0);
+15
+16    prefix[0] = diff[0];
+17    for(int i = 1; i < n; i++) {
+18        prefix[i] = prefix[i - 1] + diff[i];
+19    }
+20
+21 
+22
+23    auto isPos = [&](int mid) -> bool {
+24        int l = -1;
+25        for(int r = mid - 1; r < n; r++) {
+26            
+27            long long chocolatesNeed = prefix[r] - (l < 0 ? 0 : prefix[l]);
+28            long long cost = abs(chocolatesNeed - 1LL * (r - l) * diff[r]); 
+29
+30            if(cost <= k) return true;
+31            l++;
+32        }
+33
+34        return false;        
+35
+36    };
+37
+38    int high = n;
+39    int low = 1;
+40    int ans = 1;
+41
+42    while(high >= low) {
+43        int mid = (high + low) / 2;
+44        if(isPos(mid)) {
+45            ans = mid;
+46            low = mid + 1;
+47        } else {
+48            high = mid - 1;
+49        }
+50    }
+51    return ans;
+52}
+53    int maxFrequency(vector<int>& nums, int k) {
+54        return maxEqualChocolates(nums, k);
+55    }
+56};
