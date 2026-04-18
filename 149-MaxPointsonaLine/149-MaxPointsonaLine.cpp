902666e56961b2bc63a81c5d471fd86971b3da9e
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-04-19 00:41:22 +0530

    File updated at 4/19/2026, 12:41:21 AM

--- 149-MaxPointsonaLine/149-MaxPointsonaLine.cpp
+++ 149-MaxPointsonaLine/149-MaxPointsonaLine.cpp
@@ -1,28 +1,39 @@
-// Last updated: 4/19/2026, 12:37:20 AM
-1class Solution {
-2public:
-3    int maxPoints(vector<vector<int>>& points) {
-4        int n = points.size();
-5        int res = 1;
-6
-7        for(int i = 0; i < n; i++) {
-8            auto x = points[i][0], y = points[i][1];
-9            unordered_map<string, int> mp;
-10            for(int j = 0; j < n; j++) {
-11                if(i == j) continue;
-12
-13                auto u = points[j][0], v = points[j][1];
-14                int upper = (y - v);
-15                int lower = (x - u);
-16                int gcd = __gcd(lower, upper);
-17                
-18                string slope = to_string(upper / gcd)  + "/" + to_string(lower / gcd);
-19
-20                mp[slope]++;
-21                res = max(res,1 + mp[slope]);
-22            }
-23        }
-24
-25        return res;
-26    }  
-27};
+// Last updated: 4/19/2026, 12:41:21 AM
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int best = 1;
+
+        for (auto& p1 : points) {
+            int seen_slopes[5903] = {};
+
+            for (auto& p2 : points) {
+                if (&p1 == &p2) continue;
+
+                int dx = p2[0] - p1[0];
+                int dy = p2[1] - p1[1];
+                if (dx < 0) {
+                    dx = -dx;
+                    dy = -dy;
+                }
+                int div = dx > dy ? gcd(dx, dy) : gcd(dy, dx);
+                dx /= div;
+                dy /= div;
+
+                unsigned long long dx_bits = *((unsigned int*) &dx);
+                unsigned long long dy_bits = *((unsigned int*) &dy);
+                unsigned long long key = ((dx_bits << 32) | dy_bits) % 5903;
+                seen_slopes[key]++;
+
+                best = max(best, seen_slopes[key] + 1);
+            }
+        }
+
+        return best;
+    }
+
+    int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b); 
+    }
+};
