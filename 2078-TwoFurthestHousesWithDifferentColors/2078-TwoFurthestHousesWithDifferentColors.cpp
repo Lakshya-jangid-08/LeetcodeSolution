cc7fc6151ee5b8a6ddfbfda133f885d078b8d242
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-04-20 09:54:04 +0530

    File updated at 4/20/2026, 9:54:02 AM

--- 2078-TwoFurthestHousesWithDifferentColors/2078-TwoFurthestHousesWithDifferentColors.cpp
+++ 2078-TwoFurthestHousesWithDifferentColors/2078-TwoFurthestHousesWithDifferentColors.cpp
@@ -1,14 +1,17 @@
-// Last updated: 4/20/2026, 9:53:15 AM
-1class Solution {
-2public:
-3    int maxDistance(vector<int>& colors) {
-4        int n = colors.size();
-5        int ans = -1;
-6        for(int i = 0; i < n; i++) {
-7            for(int j = 0; j < n; j++) {
-8                if(colors[i] != colors[j]) ans = max(ans, abs(i - j));
-9            }
-10        }
-11        return ans;
-12    }
-13};
+// Last updated: 4/20/2026, 9:54:02 AM
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int n = colors.size();
+        int ans = 0;
+        int i=0; int j=n-1;
+        while(colors[i]==colors[j]) j--;
+        ans = max(ans,j-i);
+
+        j = n-1;
+        while(colors[i]==colors[j]) i++;
+        ans = max(ans,j-i);
+
+        return ans;
+    }
+};
