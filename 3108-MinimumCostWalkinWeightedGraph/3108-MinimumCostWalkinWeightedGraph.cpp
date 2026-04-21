96ec23866896066c0ff39e49e2c5760be74675c4
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-04-21 21:43:33 +0530

    File updated at 4/21/2026, 9:43:32 PM

--- 3108-MinimumCostWalkinWeightedGraph/3108-MinimumCostWalkinWeightedGraph.cpp
+++ 3108-MinimumCostWalkinWeightedGraph/3108-MinimumCostWalkinWeightedGraph.cpp
@@ -1,51 +1,48 @@
-// Last updated: 4/21/2026, 9:37:01 PM
+// Last updated: 4/21/2026, 9:43:32 PM
 1class Solution {
 2public:
 3    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
-4        int val = 1 << 20;
-5        val--;
-6        vector<int> parent(n), rank(n, 0), weight(n, val);
-7        iota(parent.begin(), parent.end(), 0);
-8        
-9        const auto find = [&](auto && self, int x) -> int {
-10            if(parent[x] == x) return x;
-11            return parent[x] = self(self, parent[x]);
-12        };
+4        vector<int> parent(n, -1), rank(n, 0), weight(n, -1);
+5        
+6        const auto find = [&](auto && self, int node) -> int {
+7            if(parent[node] == -1) return node;
+8            return parent[node] = self(self, parent[node]);
+9        };
+10
+11        const auto unite = [&](int u, int v, int w) {
+12            int pu = find(find, u), pv = find(find, v);
 13
-14        const auto unite = [&](int u, int v, int w) {
-15            int pu = find(find, u), pv = find(find, v);
-16
-17            if(pu == pv) {
-18                weight[pv] = weight[pv] & w;
-19                return;
-20            }
-21
-22            if(rank[pu] < rank[pv]) {
-23                rank[pv] += rank[pu];
-24                parent[pu] = pv;
-25                weight[pv] = weight[pu] & weight[pv] & w;
-26            } else {
-27                rank[pu] += rank[pv];
-28                parent[pv] = pu;
-29                weight[pu] = weight[pu] & weight[pv] & w;
-30            }
-31            return;
-32        };
-33
-34        for(auto &e : edges) {
-35            int u = e[0], v = e[1], w = e[2];
-36            unite(u,v,w);
-37        }
-38
-39        vector<int> res;
-40        for(auto &q  : query) {
-41            int src = q[0], dst = q[1];
-42            int ps = find(find, src) , pd = find(find, dst);
-43            if(ps != pd) res.push_back(-1);
-44            else {
-45                res.push_back(weight[ps]);
-46            }
-47        }
-48        return res;
-49    }
-50};
+14            if(pu == pv) {
+15                weight[pv] = weight[pv] & w;
+16                return;
+17            }
+18
+19            if(rank[pu] < rank[pv]) {
+20                rank[pv] += rank[pu];
+21                parent[pu] = pv;
+22                weight[pv] = weight[pu] & weight[pv] & w;
+23            } else {
+24                rank[pu] += rank[pv];
+25                parent[pv] = pu;
+26                weight[pu] = weight[pu] & weight[pv] & w;
+27            }
+28            return;
+29        };
+30
+31        for(auto &e : edges) {
+32            int u = e[0], v = e[1], w = e[2];
+33            unite(u,v,w);
+34        }
+35
+36        vector<int> res;
+37        for(auto &q  : query) {
+38            int src = q[0], dst = q[1];
+39            int ps = find(find, src) , pd = find(find, dst);
+40            if(ps != pd) res.push_back(-1);
+41            else {
+42                res.push_back(weight[ps]);
+43            }
+44        }
+45        return res;
+46    }
+47};
