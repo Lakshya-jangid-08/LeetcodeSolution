6a9fef615ed6b994568a97c3675d80b90480e4b7
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-04-21 21:47:31 +0530

    File updated at 4/21/2026, 9:47:31 PM

--- 3108-MinimumCostWalkinWeightedGraph/3108-MinimumCostWalkinWeightedGraph.cpp
+++ 3108-MinimumCostWalkinWeightedGraph/3108-MinimumCostWalkinWeightedGraph.cpp
@@ -1,48 +1,61 @@
-// Last updated: 4/21/2026, 9:43:32 PM
-1class Solution {
-2public:
-3    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
-4        vector<int> parent(n, -1), rank(n, 0), weight(n, -1);
-5        
-6        const auto find = [&](auto && self, int node) -> int {
-7            if(parent[node] == -1) return node;
-8            return parent[node] = self(self, parent[node]);
-9        };
-10
-11        const auto unite = [&](int u, int v, int w) {
-12            int pu = find(find, u), pv = find(find, v);
-13
-14            if(pu == pv) {
-15                weight[pv] = weight[pv] & w;
-16                return;
-17            }
-18
-19            if(rank[pu] < rank[pv]) {
-20                rank[pv] += rank[pu];
-21                parent[pu] = pv;
-22                weight[pv] = weight[pu] & weight[pv] & w;
-23            } else {
-24                rank[pu] += rank[pv];
-25                parent[pv] = pu;
-26                weight[pu] = weight[pu] & weight[pv] & w;
-27            }
-28            return;
-29        };
-30
-31        for(auto &e : edges) {
-32            int u = e[0], v = e[1], w = e[2];
-33            unite(u,v,w);
-34        }
-35
-36        vector<int> res;
-37        for(auto &q  : query) {
-38            int src = q[0], dst = q[1];
-39            int ps = find(find, src) , pd = find(find, dst);
-40            if(ps != pd) res.push_back(-1);
-41            else {
-42                res.push_back(weight[ps]);
-43            }
-44        }
-45        return res;
-46    }
-47};
+// Last updated: 4/21/2026, 9:47:31 PM
+#pragma GCC optimize("Ofast,unroll-loops")
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+static const int fast_io = []() {
+    ios_base::sync_with_stdio(0); cin.tie(0); return 0;
+}();
+
+class Solution {
+    int parent[100005];
+    int component_and[100005];
+
+    int find(int i) {
+        if (parent[i] == i) return i;
+        return parent[i] = find(parent[i]);
+    }
+
+    void unite(int i, int j, int w) {
+        int root_i = find(i);
+        int root_j = find(j);
+        if (root_i != root_j) {
+            parent[root_i] = root_j;
+            component_and[root_j] &= component_and[root_i];
+        }
+        // В любом случае обновляем AND компоненты текущим ребром
+        component_and[root_j] &= w;
+    }
+
+public:
+    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
+        // Инициализация
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+            component_and[i] = (1 << 20) - 1; // Заполняем единицами (макс. вес 10^5 < 2^17)
+        }
+
+        for (const auto& e : edges) {
+            unite(e[0], e[1], e[2]);
+        }
+
+        vector<int> res;
+        res.reserve(query.size());
+
+        for (const auto& q : query) {
+            int s = q[0], t = q[1];
+            int root_s = find(s);
+            int root_t = find(t);
+
+            if (root_s != root_t) {
+                res.push_back(-1);
+            } else {
+                res.push_back(component_and[root_s]);
+            }
+        }
+
+        return res;
+    }
+};
