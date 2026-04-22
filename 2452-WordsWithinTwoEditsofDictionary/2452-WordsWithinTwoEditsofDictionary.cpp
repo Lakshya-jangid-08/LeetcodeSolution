b450a52bf8e9b8fe2c35ed5ab2d4b4e1c1a2ca25
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-04-22 09:46:23 +0530

    File updated at 4/22/2026, 9:46:23 AM

--- 2452-WordsWithinTwoEditsofDictionary/2452-WordsWithinTwoEditsofDictionary.cpp
+++ 2452-WordsWithinTwoEditsofDictionary/2452-WordsWithinTwoEditsofDictionary.cpp
@@ -1,26 +1,24 @@
-// Last updated: 4/22/2026, 9:41:44 AM
-1class Solution {
-2public:
-3    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
-4        vector<string> ans;
-5        int n = queries[0].length();
-6        const auto isMatch = [&](string s, string t) -> bool {
-7            if(s == t) return true;
-8            int cnt = 0;
-9            for(int i = 0; i < n; i++) {
-10                if(s[i] != t[i]) cnt++; 
-11            }
-12            return (cnt <= 2);
-13        };
-14
-15        for (auto &query : queries) {
-16            for (auto &s : dictionary) {
-17                if (isMatch(s, query)) {
-18                    ans.push_back(query);
-19                    break;
-20                }
-21            }
-22        }
-23        return ans;
-24    }
-25};
+// Last updated: 4/22/2026, 9:46:23 AM
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        int n=queries[0].size();
+        vector<string> ans;
+        for(auto &it:queries){
+            for(auto &i:dictionary){
+                int diff=0;
+                for(int idx=0; idx<n; idx++){
+                    if(it[idx]!=i[idx]){
+                        diff++;
+                    }
+                    if(diff>2) break;
+                }
+                if(diff<=2){
+                    ans.push_back(it);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
