8b3389ced2e70bf56d8cff6ca960dc815b011d3c
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-04-23 17:34:04 +0530

    File updated at 4/23/2026, 5:34:04 PM

--- 6-ZigzagConversion/6-ZigzagConversion.cpp
+++ 6-ZigzagConversion/6-ZigzagConversion.cpp
@@ -1,20 +1,31 @@
-// Last updated: 4/23/2026, 5:32:47 PM
-1class Solution {
-2public:
-3    string convert(string s, int numRows) {
-4        if(numRows == 1) return s;
-5        vector<string> strs(numRows + 1, "");
-6        int idx = 0;
-7        int inc = 0;
-8        for(int i = 0; i < s.length(); i++) {
-9            strs[idx].push_back(s[i]);
-10            if(idx == numRows - 1) inc = -1;
-11            if(idx == 0) inc = 1;
-12            idx += inc;
-13        }
-14        for(int i = 1; i < numRows; i++) {
-15            strs[0] += strs[i];
-16        }
-17        return strs[0];
-18    }
-19};
+// Last updated: 4/23/2026, 5:34:04 PM
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1 || numRows >= s.length())
+            return s;
+
+        vector<string> rows(numRows);
+        int currRow = 0;
+        bool goingDown = false;
+
+        for (char c : s) {
+            rows[currRow] += c;
+
+            if (currRow == 0 || currRow == numRows - 1)
+                goingDown = !goingDown;
+
+            currRow += goingDown ? 1 : -1;
+        }
+
+        string result;
+        for (string row : rows)
+            result += row;
+
+        return result;
+    }
+};
