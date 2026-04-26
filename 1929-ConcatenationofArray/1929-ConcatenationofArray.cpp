f77778b785fd4bbc053ccb368489e66e162fca00
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-04-26 13:22:04 +0530

    File updated at 4/26/2026, 1:22:02 PM

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.cpp
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.cpp
@@ -1,18 +1,18 @@
-// Last updated: 4/26/2026, 1:20:42 PM
+// Last updated: 4/26/2026, 1:22:02 PM
 1class Solution {
 2public:
-3    int findMaxConsecutiveOnes(vector<int>& nums) {
-4        int l = -1, r = 0;
-5        int ans = 0;
-6        for(int i = 0; i < nums.size(); i++) {
-7            if(nums[i] == 1) {
-8                r = i;
-9            }
-10            else {
-11                l = i;
-12            }
-13            ans = max(ans,r-l);
+3    vector<int> findErrorNums(vector<int>& nums) {
+4        int duplicate = 0;
+5        int original = 0;
+6        int n = nums.size();
+7        vector<int> freq(n+1, 0);
+8        for(int i = 0; i < n; i++){
+9            freq[nums[i]]++;
+10        }
+11        for(int i = 1; i <= n; i++){
+12            if(freq[i] == 2) duplicate = i;
+13            if(freq[i] == 0) original = i;
 14        }
-15        return ans;
+15        return {duplicate, original};
 16    }
 17};
