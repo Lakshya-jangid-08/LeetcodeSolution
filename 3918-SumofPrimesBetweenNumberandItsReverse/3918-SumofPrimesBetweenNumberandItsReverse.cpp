eb9e839e957c1f7c0bd0aae3e262bee599743f53
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-05-03 10:36:49 +0530

    File updated at 5/3/2026, 10:36:49 AM

--- 3918-SumofPrimesBetweenNumberandItsReverse/3918-SumofPrimesBetweenNumberandItsReverse.cpp
+++ 3918-SumofPrimesBetweenNumberandItsReverse/3918-SumofPrimesBetweenNumberandItsReverse.cpp
@@ -1,25 +1,32 @@
-// Last updated: 5/3/2026, 10:36:25 AM
-1class Solution {
-2public:
-3
-4    bool isPrime(int val) {
-5        if(val == 1) return false;
-6        for(int i = 2; i < val; i++) {
-7            if(val % i == 0) return false;
-8        }
-9        return true;
-10    } 
-11    
-12    int sumOfPrimesInRange(int n) {
-13        string s = to_string(n);
-14        reverse(s.begin(), s.end());
-15        int r = stoi(s);
-16        int l = min(n, r);
-17        r = max(n, r);
-18        int sum = 0;
-19        for(int i = l; i <= r; i++) {
-20            sum += (isPrime(i) ? i : 0);
-21        }
-22        return sum;
-23    }
-24};
+// Last updated: 5/3/2026, 10:36:49 AM
+class Solution {
+public:
+    int reverseInt(int n){
+        int rev = 0;
+        while (n > 0){
+            rev = rev * 10 + n % 10;
+            n/=10;
+        }
+        return rev;
+    }
+    bool isPrime(int num){
+        if (num < 2) return false;
+        for(int i = 2; i * i <= num; i++){
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+    
+    int sumOfPrimesInRange(int n) {
+        int r = reverseInt(n);
+        int start = min(n,r);
+        int end = max(n,r);
+        int totalSum=0;
+        for(int i = start; i <=end; i++){
+            if (isPrime(i)) {
+                totalSum +=i;
+            }
+        }
+        return totalSum;
+    }
+};
