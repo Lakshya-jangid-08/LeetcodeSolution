0c53702c82919359d6f72b039cb85512116d4668
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-05-12 08:03:14 +0530

    File updated at 5/12/2026, 8:03:13 AM

--- 1665-MinimumInitialEnergytoFinishTasks/1665-MinimumInitialEnergytoFinishTasks.cpp
+++ 1665-MinimumInitialEnergytoFinishTasks/1665-MinimumInitialEnergytoFinishTasks.cpp
@@ -1,4 +1,4 @@
-// Last updated: 5/12/2026, 7:59:26 AM
+// Last updated: 5/12/2026, 8:03:13 AM
 1class Solution {
 2public:
 3    int minimumEffort(vector<vector<int>>& tasks) {
@@ -8,14 +8,12 @@
 7        });
 8        int curr = 0, need = 0;
 9        for(int i = 0; i < tasks.size(); i++) {
-10            if(curr >= tasks[i][1]) {
-11                curr -= tasks[i][0];
-12            } else {
-13                need += tasks[i][1] - curr;
-14                curr = tasks[i][1];
-15                curr -= tasks[i][0];
-16            }
-17        }
-18        return need;
-19    }
-20};
+10            if(curr < tasks[i][1]) {
+11                need += tasks[i][1] - curr;
+12                curr = tasks[i][1];
+13            }
+14            curr -= tasks[i][0];
+15        }
+16        return need;
+17    }
+18};
