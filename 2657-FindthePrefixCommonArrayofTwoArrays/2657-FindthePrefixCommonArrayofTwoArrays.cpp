2b523d60ffb36821994327a1be99734d181998c5
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-05-21 12:06:30 +0530

    File updated at 5/21/2026, 12:06:30 PM

--- 2657-FindthePrefixCommonArrayofTwoArrays/2657-FindthePrefixCommonArrayofTwoArrays.cpp
+++ 2657-FindthePrefixCommonArrayofTwoArrays/2657-FindthePrefixCommonArrayofTwoArrays.cpp
@@ -1,30 +1,21 @@
-// Last updated: 5/20/2026, 11:25:26 PM
+// Last updated: 5/21/2026, 12:06:30 PM
 1class Solution {
 2public:
 3    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
 4        int n = A.size();
-5        vector<int> prefixCommonArray(n);
-6
-7        // Loop through each index to calculate common elements for each prefix
-8        for (int currentIndex = 0; currentIndex < n; ++currentIndex) {
-9            int commonCount = 0;
-10
-11            // Compare elements in A and B within the range of current prefix
-12            for (int aIndex = 0; aIndex <= currentIndex; ++aIndex) {
-13                for (int bIndex = 0; bIndex <= currentIndex; ++bIndex) {
-14                    // Check if elements match, and count if they do
-15                    if (A[aIndex] == B[bIndex]) {
-16                        ++commonCount;
-17                        break;
-18                    }
-19                }
-20            }
-21
-22            // Store the count of common elements for the current prefix
-23            prefixCommonArray[currentIndex] = commonCount;
-24        }
-25
-26        // Return the final array with counts of common elements in each prefix
-27        return prefixCommonArray;
-28    }
-29};
+5
+6        vector<int> res(n);
+7        for(int idx = 0; idx < n; idx++) {
+8            
+9            int cnt = 0;
+10            for(int i = 0; i <= idx; i++) {
+11                for(int j = 0; j <= idx; j++) {
+12                    if(A[i] == B[j]) cnt++;
+13                }
+14            }
+15
+16            res[idx] = cnt;
+17        }
+18        return res;
+19    }
+20};
