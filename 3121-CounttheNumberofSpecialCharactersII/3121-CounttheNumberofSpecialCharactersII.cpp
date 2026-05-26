6e85894e8d961b819d0893a0204aff6900663830
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-05-26 21:14:12 +0530

    Create 3121-CounttheNumberofSpecialCharactersII

---
+++ 3121-CounttheNumberofSpecialCharactersII/3121-CounttheNumberofSpecialCharactersII.cpp
@@ -0,0 +1,26 @@
+// Last updated: 5/26/2026, 9:14:10 PM
+1class Solution {
+2public:
+3    int numberOfSpecialChars(string word) {
+4        vector<int> lowerIdx(26, -1);
+5
+6        int idx = 0;
+7        for(auto &c : word) {
+8            if(c <= 'z' && c >= 'a') lowerIdx[c - 'a'] = idx;
+9            idx++;
+10        }
+11
+12        idx = 0;
+13        int cnt = 0;
+14        for(auto &c : word) {
+15            if(c >= 'A' && c <= 'Z' && lowerIdx[c - 'A'] != -1) {
+16                cout << idx << endl;
+17                if(lowerIdx[c - 'A'] < idx) cnt++;
+18                lowerIdx[c - 'A'] = -1;
+19            }
+20            idx++;
+21        }
+22
+23        return cnt;
+24    }
+25};
