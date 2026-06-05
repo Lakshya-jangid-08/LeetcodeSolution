7033373ce3923d20166d6e8c1ecb0f49181a5350
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-06-05 17:05:37 +0530

    File updated at 6/5/2026, 5:05:32 PM

--- 3751-TotalWavinessofNumbersinRangeI/3751-TotalWavinessofNumbersinRangeI.cpp
+++ 3751-TotalWavinessofNumbersinRangeI/3751-TotalWavinessofNumbersinRangeI.cpp
@@ -1,4 +1,4 @@
-// Last updated: 6/4/2026, 12:46:53 PM
+// Last updated: 6/5/2026, 5:05:32 PM
 1class Solution {
 2public:
 3    int totalWaviness(int num1, int num2) {
@@ -17,17 +17,15 @@
 16                if(prev < curr && curr > next) cnt++;
 17                num /= 10;
 18            } 
-19        // d << endl;
-20            return cnt;
-21        };
-22
-23        int cnt = 0;
-24
-25        for(int num = max(100, num1); num <= num2; num++) {
-26            cnt += check(num);
-27            // d << cnt << endl;
-28        }
-29
-30        return cnt;
-31    }
-32};
+19            return cnt;
+20        };
+21
+22        int cnt = 0;
+23
+24        for(int num = max(100, num1); num <= num2; num++) {
+25            cnt += check(num);
+26        }
+27
+28        return cnt;
+29    }
+30};
