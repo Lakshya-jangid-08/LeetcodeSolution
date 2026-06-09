988dc3c45b44c29f31044994586b4dc74719a60c
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-06-09 10:18:35 +0530

    File updated at 6/9/2026, 10:18:35 AM

--- 3689-MaximumTotalSubarrayValueI/3689-MaximumTotalSubarrayValueI.cpp
+++ 3689-MaximumTotalSubarrayValueI/3689-MaximumTotalSubarrayValueI.cpp
@@ -1,11 +1,9 @@
-// Last updated: 6/9/2026, 10:18:06 AM
-1class Solution {
-2public:
-3    long long maxTotalValue(vector<int>& nums, int k) {
-4        long long maxElem = *max_element(nums.begin(), nums.end());
-5        long long minElem = *min_element(nums.begin(), nums.end());
-6    
-7    
-8        return 1LL * k * (maxElem - minElem);
-9    }
-10};
+// Last updated: 6/9/2026, 10:18:35 AM
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        int a = *max_element(nums.begin(), nums.end());
+        int b = *min_element(nums.begin(), nums.end());
+        return 1LL * (a - b) * k; 
+    }
+};
