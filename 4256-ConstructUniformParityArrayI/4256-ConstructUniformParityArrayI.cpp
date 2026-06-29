580271bd3d055cbcf50dd03a34071cd526f03adf
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-06-29 10:19:48 +0530

    Create 4256-ConstructUniformParityArrayI

---
+++ 4256-ConstructUniformParityArrayI/4256-ConstructUniformParityArrayI.cpp
@@ -0,0 +1,8 @@
+// Last updated: 6/29/2026, 10:19:46 AM
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        if(nums1.size() == 0) return false;
+        else                  return true;
+    }
+};
