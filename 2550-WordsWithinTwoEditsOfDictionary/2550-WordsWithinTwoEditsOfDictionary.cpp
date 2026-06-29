f12524273d31f3dd878163a423e047c3f7a084e3
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-06-29 10:21:03 +0530

    Create 2550-WordsWithinTwoEditsOfDictionary

---
+++ 2550-WordsWithinTwoEditsOfDictionary/2550-WordsWithinTwoEditsOfDictionary.cpp
@@ -0,0 +1,24 @@
+// Last updated: 6/29/2026, 10:21:01 AM
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        int n=queries[0].size();
+        vector<string> ans;
+        for(auto &it:queries){
+            for(auto &i:dictionary){
+                int diff=0;
+                for(int idx=0; idx<n; idx++){
+                    if(it[idx]!=i[idx]){
+                        diff++;
+                    }
+                    if(diff>2) break;
+                }
+                if(diff<=2){
+                    ans.push_back(it);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
