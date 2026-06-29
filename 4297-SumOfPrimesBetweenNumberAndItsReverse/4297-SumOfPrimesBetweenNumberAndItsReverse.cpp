568be5c69525039a7abd819ba50021a6c04080d2
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-06-29 10:19:19 +0530

    Create 4297-SumOfPrimesBetweenNumberAndItsReverse

---
+++ 4297-SumOfPrimesBetweenNumberAndItsReverse/4297-SumOfPrimesBetweenNumberAndItsReverse.cpp
@@ -0,0 +1,32 @@
+// Last updated: 6/29/2026, 10:19:17 AM
+class Solution {
+public:
+    int reverseInt(int n){
+        int rev = 0;
+        while (n > 0){
+            rev = rev * 10 + n % 10;
+            n/=10;
+        }
+        return rev;
+    }
+    bool isPrime(int num){
+        if (num < 2) return false;
+        for(int i = 2; i * i <= num; i++){
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+    
+    int sumOfPrimesInRange(int n) {
+        int r = reverseInt(n);
+        int start = min(n,r);
+        int end = max(n,r);
+        int totalSum=0;
+        for(int i = start; i <=end; i++){
+            if (isPrime(i)) {
+                totalSum +=i;
+            }
+        }
+        return totalSum;
+    }
+};
