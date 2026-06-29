d036903f56a192bf8b59fc3fadea521f9f94925c
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-06-29 10:21:13 +0530

    Create 2216-DeleteTheMiddleNodeOfALinkedList

---
+++ 2216-DeleteTheMiddleNodeOfALinkedList/2216-DeleteTheMiddleNodeOfALinkedList.cpp
@@ -0,0 +1,23 @@
+// Last updated: 6/29/2026, 10:21:11 AM
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if (!head->next) return nullptr;
+
+        ListNode* slow = head;
+        ListNode* fast = slow->next->next;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        slow->next = slow->next->next;
+        return head;
+    }
+};
+
+
+
+// Memory Deallocation
+// LeetCode environment automatically frees the bypassed middle node
