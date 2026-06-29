c9422a66bcb27662b965d7f7801d108820286b15
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-06-29 10:23:31 +0530

    Create 123-BestTimeToBuyAndSellStockIii

---
+++ 123-BestTimeToBuyAndSellStockIii/123-BestTimeToBuyAndSellStockIii.cpp
@@ -0,0 +1,31 @@
+// Last updated: 6/29/2026, 10:23:29 AM
+class Solution {
+public:
+    int maxProfit(vector<int>& nums) {
+        int n = nums.size();   
+        int dp[n + 1][2][3];
+
+        memset(dp, 0, sizeof(dp));
+
+
+        for(int idx = n - 1; idx >= 0; idx--) {
+             for(int buy = 1; buy >= 0; buy--) {
+                for(int trans = 1; trans >= 0; trans--) {
+                    int profit = 0;
+                
+                    if(buy) {
+                        profit = dp[idx + 1][0][trans] - nums[idx];
+                    } else {
+                        profit = dp[idx + 1][1][trans + 1] + nums[idx];
+                    }
+                
+                    dp[idx][buy][trans] =  max(profit, dp[idx + 1][buy][trans]);
+                
+                }
+            }
+        }
+
+        return dp[0][1][0];
+
+    }
+};
