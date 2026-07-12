c8b813867b01e4621c997d4161cbfb0099579191
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-07-12 18:28:46 +0530

    File updated at 7/12/2026, 6:28:44 PM

--- 3620-NetworkRecoveryPathways/3620-NetworkRecoveryPathways.cpp
+++ 3620-NetworkRecoveryPathways/3620-NetworkRecoveryPathways.cpp
@@ -1,77 +1,53 @@
-// Last updated: 7/12/2026, 12:21:30 PM
-1
-2struct node {
-3    long long weight;
-4    int num;
-5
-6    bool operator<(const node& other) const {
-7        return weight > other.weight;
-8    }
-9
-10};
-11
-12class Solution {
-13public:
-14    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online, long long k) {
-15        // DAG - toposort 
-16        // Dijsktra
-17        int n = online.size();
-18        vector<vector<pair<int,int>>> adj(n);
-19
-20        int mxw = -1, mnw = 1e9 + 1; 
-21
-22        for(const auto & ed : edges) {
-23            int u = ed[0], v = ed[1], w = ed[2];
-24            if(online[u] && online[v]) {
-25                mxw = max(mxw, w);
-26                mnw = min(mnw, w);
-27                adj[u].push_back({v,w});
-28            }
-29        }
-30
-31        // min should be maximum
+// Last updated: 7/12/2026, 6:28:44 PM
+1class unionSet {
+2 public:
+3    vector<int> parent, size, mnw;
+4
+5    unionSet(int n) : parent(n + 1), size(n + 1, 1), mnw(n + 1, 1e9) {
+6        iota(parent.begin(), parent.end(), 0);
+7    }
+8
+9    int findPar(int x) {
+10        if(parent[x] == x) return x;
+11        return parent[x] = findPar(parent[x]);
+12    }
+13
+14    void join(int u, int v, int w) {
+15        int pu = findPar(u), pv = findPar(v);
+16        if(pu == pv){
+17            mnw[pu] = mnw[pv] = min(mnw[pu], w);
+18            return;
+19        }
+20
+21        if(size[pu] < size[pv]) {
+22            size[pv] += size[pu];
+23            parent[pu] = pv;
+24        } else {
+25            parent[pv] = pu;
+26            size[pu] += size[pv];
+27        }
+28        int minW = min({mnw[pv], mnw[pu], w});
+29        mnw[pv] = mnw[pu] = minW;
+30        return;
+31    }
 32
-33        const auto isPos = [&](int minw) -> bool {
-34            vector<long long> dist(n, LLONG_MAX);
-35            priority_queue<node> pq;
-36            pq.push({0LL, 0});
+33    int solution() {
+34        return mnw[findPar(1)];
+35    }
+36};
 37
-38            dist[0] = 0;
-39
-40            while(!pq.empty()) {
-41
-42                auto topNode = pq.top(); pq.pop();
-43                long long weight = topNode.weight; 
-44                int num = topNode.num;
-45
-46                if(weight > dist[num]) continue;
-47                if(num == n-1) return true;
+38class Solution {
+39 public:
+40    int minScore(int n, vector<vector<int>>& roads) {
+41        
+42        unionSet ds(n);
+43
+44        for(const auto &road : roads) {
+45            const int u = road[0], v = road[1], w = road[2];
+46            ds.join(u, v, w);
+47        }
 48
-49                for(const auto &[nextNum, w] : adj[num]) {
-50                    if(1LL * w + weight > 1LL * k) continue;
-51                    if(w < minw) continue;
-52                    if(dist[nextNum] < dist[num] + w) continue;
-53                    dist[nextNum] = dist[num] + w;
-54                    pq.push({1LL * w + weight, nextNum});
-55                }
-56
-57            }   
-58            return false;
-59        };
-60
-61        if(!isPos(0)) return -1;
-62
-63        int res = -1;
-64        while(mxw >= mnw) {
-65            int mid = (mxw) - (mxw - mnw) / 2;
-66            if(isPos(mid)) {
-67                mnw = mid + 1;
-68                res = mid;
-69            } else {
-70                mxw = mid - 1;
-71            }
-72        }
-73
-74        return res;
-75    }
-76};
+49        return ds.solution();
+50
+51    }
+52};
