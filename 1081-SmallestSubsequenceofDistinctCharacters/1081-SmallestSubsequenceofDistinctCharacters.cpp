f332539d5f6a23653074dfe6d987908f0f66e802
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-07-22 11:46:58 +0530

    Create 1081-SmallestSubsequenceofDistinctCharacters

---
+++ 1081-SmallestSubsequenceofDistinctCharacters/1081-SmallestSubsequenceofDistinctCharacters.cpp
@@ -0,0 +1,32 @@
+// Last updated: 7/22/2026, 11:46:56 AM
+1class Solution {
+2public:
+3    string smallestSubsequence(string s) {
+4        vector<int> freq(26, 0);
+5
+6        for(char &c : s) {
+7            freq[c - 'a'] += 1;
+8        }
+9
+10        vector<int> vis(26);
+11
+12        string st;
+13        for(char c : s) {
+14            // did not select yet
+15            if(!vis[c - 'a']) {
+16                while(!st.empty() && st.back() > c) {
+17                    if(freq[st.back() - 'a'] > 0) {
+18                        vis[st.back() - 'a'] = 0;
+19                        st.pop_back();
+20                    } else {
+21                        break;
+22                    }
+23                }
+24                vis[c - 'a'] = 1;
+25                st.push_back(c);
+26            }
+27            freq[c - 'a']--;
+28        }
+29        return st;
+30    }
+31};
