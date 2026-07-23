e8d47b371a97fa9638d9e892eebaea57a6e28ca5
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-07-23 16:06:49 +0530

    File updated at 7/23/2026, 4:06:47 PM

--- 3513-NumberofUniqueXORTripletsI/3513-NumberofUniqueXORTripletsI.cpp
+++ 3513-NumberofUniqueXORTripletsI/3513-NumberofUniqueXORTripletsI.cpp
@@ -1,12 +1,16 @@
-// Last updated: 7/23/2026, 3:56:42 PM
+// Last updated: 7/23/2026, 4:06:47 PM
 1class Solution {
 2public:
 3    int uniqueXorTriplets(vector<int>& nums) {
-4        int pow2[] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072};
-5        int n = nums.size();
-6
-7        if(n == 2) return 2;
-8        if(n == 1) return 1;
-9        return *lower_bound(pow2, pow2 + 18, n + 1);
-10    }
-11};
+4        size_t n = nums.size();
+5        int m = n;
+6        
+7        m |= m >> 1;
+8        m |= m >> 2;
+9        m |= m >> 4;
+10        m |= m >> 8;
+11        m |= m >> 16;
+12        
+13        return (m + 1) >> (3 / (n + 1));
+14    }
+15};
