f42ef8cdb70136ce8ab73eaeb71c271530ee45b8
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-07-24 21:55:33 +0530

    File updated at 7/24/2026, 9:55:33 PM

--- 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.cpp
+++ 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.cpp
@@ -1,23 +1,33 @@
-// Last updated: 7/23/2026, 4:31:44 PM
-1/**
-2 * Definition for a binary tree node.
-3 * struct TreeNode {
-4 *     int val;
-5 *     TreeNode *left;
-6 *     TreeNode *right;
-7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-10 * };
-11 */
-12class Solution {
-13public:
-14    vector<int> temp;
-15    vector<int> preorderTraversal(TreeNode* root) {
-16        if(!root) return temp;
-17        temp.push_back(root->val);
-18        preorderTraversal(root->left);
-19        preorderTraversal(root->right);
-20        return temp;
-21    }
-22};
+// Last updated: 7/24/2026, 9:55:33 PM
+1class Solution {
+2public: // N L R
+3    vector<int> preorderTraversal(TreeNode* root) {
+4        if(!root) return {};
+5        vector<int> res;
+6        auto curr = root;
+7        
+8        while(curr) {
+9            if(curr->left) {
+10                // right-most node;
+11                auto prev = curr->left;
+12                while(prev && prev->right && prev->right != curr) {
+13                    prev = prev->right;
+14                } 
+15
+16                if(prev->right) {
+17                    curr = curr->right;
+18                    prev->right = nullptr;
+19                } else {
+20                    res.push_back(curr->val);
+21                    prev->right = curr;
+22                    curr = curr->left;
+23                }
+24
+25            } else {
+26                res.push_back(curr->val);
+27                curr = curr->right;
+28            }
+29        }
+30        return res;
+31    }
+32};
