899717e065ac92cba3e314e1808b0398bd276ec2
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-07-27 22:37:21 +0530

    File updated at 7/27/2026, 10:37:19 PM

--- 472-ConcatenatedWords/472-ConcatenatedWords.cpp
+++ 472-ConcatenatedWords/472-ConcatenatedWords.cpp
@@ -1,22 +1,45 @@
-// Last updated: 7/27/2026, 10:34:37 PM
+// Last updated: 7/27/2026, 10:37:19 PM
 1class Solution {
-2public:
-3    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-4        unordered_set<string> dic(words.begin(), words.end());
-5        vector<string> res;
-6
-7        for(auto &word : words) {
-8            int m = word.size();
-9            vector<bool> dp(m + 1, false);
-10            dp[0] = true;
-11            for(int i = 1; i <= m; i++) {
-12                for(int j = (i == m ? 1 : 0); !dp[i] && j < i; j++) {
-13                    dp[i] = dp[j] && dic.count(word.substr(j, i - j));
-14                }
-15            }
-16            if(dp[m]) res.push_back(word);
-17        }
-18
-19        return res;
-20    }
-21};
+2    bool solve(string &s, int idx, unordered_set<string> &st, vector<int> &dp) {
+3        if (idx == s.size())
+4            return true;
+5
+6        if (dp[idx] != -1)
+7            return dp[idx];
+8
+9        string cur;
+10
+11        for (int i = idx; i < s.size(); i++) {
+12            cur.push_back(s[i]);
+13
+14            if (st.count(cur)) {
+15                if (solve(s, i + 1, st, dp))
+16                    return dp[idx] = true;
+17            }
+18        }
+19
+20        return dp[idx] = false;
+21    }
+22
+23public:
+24    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+25
+26        unordered_set<string> st(words.begin(), words.end());
+27
+28        vector<string> ans;
+29
+30        for (string &word : words) {
+31
+32            st.erase(word);               // don't allow using itself
+33
+34            vector<int> dp(word.size(), -1);
+35
+36            if (solve(word, 0, st, dp))
+37                ans.push_back(word);
+38
+39            st.insert(word);
+40        }
+41
+42        return ans;
+43    }
+44};
