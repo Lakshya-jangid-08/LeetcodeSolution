93f4407fdeeba1854929fa2b255478362f3e2d9a
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-07-28 14:26:38 +0530

    File updated at 7/28/2026, 2:26:37 PM

--- 3517-SmallestPalindromicRearrangementI/3517-SmallestPalindromicRearrangementI.cpp
+++ 3517-SmallestPalindromicRearrangementI/3517-SmallestPalindromicRearrangementI.cpp
@@ -1,16 +1,22 @@
-// Last updated: 7/28/2026, 10:49:40 AM
+// Last updated: 7/28/2026, 2:26:37 PM
 1class Solution {
 2public:
 3    string smallestPalindrome(string s) {
-4        int len = s.length();
-5        int partition = len / 2;
+4        int n = s.length();
+5        int bucket[26] = {0};
 6
-7        sort(s.begin(), s.begin() + partition);
-8        cout << s ;
-9        for (int i = 0; i < partition; ++i) {
-10            s[len - 1 - i] = s[i];
-11        }
-12
-13        return s;
-14    }
-15};
+7        for(int i = 0; i < n / 2; i++) {
+8            int c = s[i] - 'a';
+9            bucket[c]++;
+10        }
+11    
+12        int idx = 0;
+13        for(int i = 0; i < 26; i++) {
+14            while(bucket[i] > 0) {
+15                s[idx] = s[n - 1 - idx] = i + 'a';
+16                bucket[i]--; idx++;
+17            }
+18        }
+19        return s;
+20    }
+21};
