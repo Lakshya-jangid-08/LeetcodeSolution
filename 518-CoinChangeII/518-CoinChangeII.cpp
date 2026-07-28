80af7f91490457e830dc9148fe45fb50715a0af4
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-07-28 14:50:01 +0530

    File updated at 7/28/2026, 2:50:00 PM

--- 518-CoinChangeII/518-CoinChangeII.cpp
+++ 518-CoinChangeII/518-CoinChangeII.cpp
@@ -1,15 +1,15 @@
-// Last updated: 7/28/2026, 2:46:26 PM
+// Last updated: 7/28/2026, 2:50:00 PM
 1class Solution {
 2public:
 3    int change(int amount, vector<int>& coins) {
 4        int n = coins.size();
-5        vector<unordered_map<int, int>> dp(n);
+5        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
 6
 7        const auto solve = [&](auto &&self, int idx, int cash) -> int {
 8            if(idx == n) return (cash == 0);
 9            if(cash < 0) return 0;
 10
-11            if(dp[idx].count(cash)) return dp[idx][cash];
+11            if(dp[idx][cash] != -1) return dp[idx][cash];
 12
 13            int take = self(self, idx, cash - coins[idx]);            
 14            int skip = self(self, idx + 1, cash);
