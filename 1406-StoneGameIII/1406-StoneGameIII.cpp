670b9c4f5b5ed709a173a4f63c5298d1bb194642
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-08-03 20:44:38 +0530

    Create 1406-StoneGameIII

---
+++ 1406-StoneGameIII/1406-StoneGameIII.cpp
@@ -0,0 +1,27 @@
+// Last updated: 8/3/2026, 8:44:35 PM
+1class Solution {
+2public:
+3    string stoneGameIII(vector<int>& stoneValue) {
+4        int n = stoneValue.size();
+5
+6        vector<int> dp(n, -1e8);
+7
+8        const auto solution = [&](auto &&self, int idx) -> int {
+9            if(idx >= n) return 0;
+10
+11            int &res = dp[idx];
+12            if(res != -1e8) return res;
+13            int values = 0;
+14            for(int inc = 0; inc + idx < n && inc < 3; inc++) {
+15                values += stoneValue[idx + inc];
+16                res = max(res, values - self(self, idx + inc + 1));
+17            }
+18            return res;
+19        };
+20
+21        int val = solution(solution, 0);
+22        cout << val << endl;
+23        return val > 0 ? "Alice" : (val < 0 ? "Bob" : "Tie"); 
+24
+25    }
+26};
