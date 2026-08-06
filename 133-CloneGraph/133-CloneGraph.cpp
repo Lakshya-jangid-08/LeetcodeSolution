c398bc90ea9d10ddb19cf60638fdbc65fc555e68
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-08-06 13:19:00 +0530

    Create 133-CloneGraph

---
+++ 133-CloneGraph/133-CloneGraph.cpp
@@ -0,0 +1,29 @@
+// Last updated: 8/6/2026, 1:18:59 PM
+1class Solution {
+2public:
+3    Node* cloneGraph(Node* node) {
+4        if (!node) return nullptr;
+5
+6        unordered_map<Node*, Node*> mp;
+7
+8        auto solve = [&](auto&& self, Node* n, Node*& r) -> void {
+9            if(mp.count(n)) {
+10                r = mp[n];
+11                return;
+12            }
+13
+14            r = new Node(n->val);
+15            mp[n] = r;
+16
+17            for (Node* x : n->neighbors) {
+18                Node* child = nullptr;
+19                self(self, x, child);
+20                r->neighbors.push_back(child);
+21            }
+22        };
+23
+24        Node* res = nullptr;
+25        solve(solve, node, res);
+26        return res;
+27    }
+28};
