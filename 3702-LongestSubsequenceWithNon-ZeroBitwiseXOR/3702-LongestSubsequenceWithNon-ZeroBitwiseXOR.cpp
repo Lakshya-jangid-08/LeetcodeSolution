bf2f6de0526f6f336170b1140de034cb72b2bc78
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-08-15 10:02:50 +0530

    File updated at 8/15/2026, 10:02:50 AM

--- 3702-LongestSubsequenceWithNon-ZeroBitwiseXOR/3702-LongestSubsequenceWithNon-ZeroBitwiseXOR.cpp
+++ 3702-LongestSubsequenceWithNon-ZeroBitwiseXOR/3702-LongestSubsequenceWithNon-ZeroBitwiseXOR.cpp
@@ -1,16 +1,16 @@
-// Last updated: 6/4/2026, 10:19:16 pm
-class Solution {
-public:
-    int longestSubsequence(vector<int>& nums) {
-        int totalXor = 0, nonZero = 0, n = nums.size();
-
-        for (int& num : nums) {
-            totalXor ^= num;
-
-            if (num != 0)
-                nonZero = 1;
-        }
-
-        return totalXor ? n : nonZero ? n - 1 : 0;
-    }
-};
+// Last updated: 8/15/2026, 10:02:50 AM
+1class Solution {
+2public:
+3    int longestSubsequence(vector<int>& nums) {
+4        int XOR = nums[0];
+5        int isNonZero = (nums[0] > 0) ? 1 : 0;
+6        for(int i = 1; i < nums.size(); i++) {
+7            XOR ^= nums[i];
+8            if(nums[i] > 0) isNonZero = 1;
+9        }
+10
+11        if(!isNonZero) return 0;
+12
+13        return (XOR > 0) ? nums.size() : nums.size() - 1;
+14    }
+15};
