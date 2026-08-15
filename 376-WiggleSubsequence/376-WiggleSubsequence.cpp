572acacf9ca972e6c256548b7bf0382dbf06050c
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-08-15 19:04:30 +0530

    File updated at 8/15/2026, 7:04:29 PM

--- 376-WiggleSubsequence/376-WiggleSubsequence.cpp
+++ 376-WiggleSubsequence/376-WiggleSubsequence.cpp
@@ -1,34 +1,80 @@
-// Last updated: 8/15/2026, 6:57:07 PM
+// Last updated: 8/15/2026, 7:04:29 PM
 1class Solution {
 2public:
-3    int wiggleMaxLength(vector<int>& nums) {
+3    vector<int> nextGreaterIndex(vector<int>& nums) {
 4        int n = nums.size();
-5        vector<vector<int>> dp(n, vector<int>(2, -1));
-6        const auto solve = [&](auto &&self, int idx, int isUp) -> int {
-7            if(idx == n) return 0;
-8            int &res = dp[idx][isUp];
-9            if(res != -1) return res;
-10
-11            res = 0;
-12            for(int i = idx + 1; i < n; i++) {
-13                int cnt = 0;
-14                if(isUp && nums[idx] < nums[i]) {
-15                    cnt = 1 + self(self, i, !isUp);
-16                } 
-17                if(!isUp && nums[idx] > nums[i]) {
-18                    cnt = 1 + self(self, i, !isUp);
-19                }
-20                res = max(res, cnt);
-21            }
-22            return res;
-23        };
-24
-25        int res = 0;
-26        for(int i = 0; i < n; i++) {
-27            int a = 1 + solve(solve, i, 1);
-28            int b = 1 + solve(solve, i, 0);
-29            res = max({res, a, b});
-30        }
-31        return res;
-32    }
-33};
+5        vector<int> nge(n, n);
+6        stack<int> st; // stores indices
+7
+8        for (int i = n - 1; i >= 0; i--) {
+9            while (!st.empty() && nums[st.top()] <= nums[i]) {
+10                st.pop();
+11            }
+12
+13            if (!st.empty())
+14                nge[i] = st.top();
+15
+16            st.push(i);
+17        }
+18
+19        return nge;
+20    }
+21    vector<int> nextSmallerIndex(vector<int>& nums) {
+22        int n = nums.size();
+23        vector<int> nse(n, n);
+24        stack<int> st; // stores indices
+25
+26        for (int i = n - 1; i >= 0; i--) {
+27            while (!st.empty() && nums[st.top()] >= nums[i]) {
+28                st.pop();
+29            }
+30
+31            if (!st.empty())
+32                nse[i] = st.top();
+33
+34            st.push(i);
+35        }
+36
+37        return nse;
+38    }
+39
+40
+41    int wiggleMaxLength(vector<int>& nums) {
+42        int n = nums.size();
+43        vector<vector<int>> dp(n, vector<int>(2, -1));
+44
+45        vector<int> nge = nextGreaterIndex(nums);
+46        vector<int> nse = nextSmallerIndex(nums);
+47
+48        const auto solve = [&](auto &&self, int idx, int isUp) -> int {
+49            if(idx == n) return 0;
+50
+51            int &res = dp[idx][isUp];
+52            if(res != -1) return res;
+53
+54            res = 0;
+55            if(isUp) {
+56                int nextIdx = nge[idx];
+57                while(nextIdx < n) {
+58                    res = max(res, 1 + self(self, nextIdx, 0));
+59                    nextIdx = nge[nextIdx];
+60                }
+61            } else {
+62                int nextIdx = nse[idx];
+63                while(nextIdx < n) {
+64                    res = max(res, 1 + self(self, nextIdx, 1));
+65                    nextIdx = nse[nextIdx];
+66                }
+67            }
+68            return res;
+69        };
+70
+71        int res = 0;
+72        for(int i = 0; i < n; i++) {
+73            int a = 1 + solve(solve, i, 1);
+74            int b = 1 + solve(solve, i, 0);
+75            res = max({res, a, b});
+76        }
+77        return res;
+78    }
+79};
