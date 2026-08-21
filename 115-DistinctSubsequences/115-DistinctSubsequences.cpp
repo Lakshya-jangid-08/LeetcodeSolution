6268f3691487a41deb7e46323af8b31117a7f224
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-08-21 17:03:54 +0530

    File updated at 8/21/2026, 5:03:53 PM

--- 115-DistinctSubsequences/115-DistinctSubsequences.cpp
+++ 115-DistinctSubsequences/115-DistinctSubsequences.cpp
@@ -1,26 +1,28 @@
-// Last updated: 8/21/2026, 4:56:34 PM
+// Last updated: 8/21/2026, 5:03:53 PM
 1class Solution {
 2public:
 3    int numDistinct(string s, string t) {
-4        
-5        vector<vector<int>> dp(s.length(), vector<int>(t.length(), -1));
+4        int n = s.length(), m = t.length();
+5        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, 0));
 6
-7        const auto solve = [&](auto && sol, int n, int m) -> int {
-8            if(m == t.length()) return 1;
-9            if(n == s.length()) return 0;
-10
-11            int &res = dp[n][m];
-12            if(res != -1) return res;
+7        for(int j = 0; j <= m; j++) {
+8            dp[n][j] = 0;
+9        }
+10        for(int i = 0; i <= n; i++) {
+11            dp[i][m] = 1;
+12        }
 13
-14            res = 0;
-15            if(s[n] == t[m]) {
-16                res += sol(sol, n + 1, m + 1);
-17            } 
-18            res += sol(sol, n + 1, m);
-19
-20            return res;
-21        };
-22
-23        return solve(solve, 0, 0);
-24    }
-25};
+14        for(int i = n - 1; i >= 0; i--) {
+15            for(int j = m - 1; j >= 0; j--) {
+16                int res = 0;
+17                if(s[i] == t[j]) {
+18                    res = res + dp[i + 1][j + 1]; 
+19                }
+20                res = res + dp[i + 1][j];
+21                dp[i][j] = res;
+22            }
+23        }
+24
+25        return dp[0][0];
+26    }
+27};
