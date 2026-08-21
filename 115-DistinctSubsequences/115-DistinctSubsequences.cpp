b3b59114f4c8c467e1b007520723eeb1cfb831be
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-08-21 17:13:33 +0530

    File updated at 8/21/2026, 5:13:32 PM

--- 115-DistinctSubsequences/115-DistinctSubsequences.cpp
+++ 115-DistinctSubsequences/115-DistinctSubsequences.cpp
@@ -1,28 +1,23 @@
-// Last updated: 8/21/2026, 5:03:53 PM
+// Last updated: 8/21/2026, 5:13:32 PM
 1class Solution {
 2public:
 3    int numDistinct(string s, string t) {
 4        int n = s.length(), m = t.length();
-5        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, 0));
-6
-7        for(int j = 0; j <= m; j++) {
-8            dp[n][j] = 0;
-9        }
-10        for(int i = 0; i <= n; i++) {
-11            dp[i][m] = 1;
-12        }
-13
-14        for(int i = n - 1; i >= 0; i--) {
-15            for(int j = m - 1; j >= 0; j--) {
-16                int res = 0;
-17                if(s[i] == t[j]) {
-18                    res = res + dp[i + 1][j + 1]; 
-19                }
-20                res = res + dp[i + 1][j];
-21                dp[i][j] = res;
-22            }
-23        }
-24
-25        return dp[0][0];
-26    }
-27};
+5        vector<unsigned long long>prev(m + 1, 0), curr(m + 1, 0);
+6        
+7        prev[m] = 1;
+8        for(int i = n - 1; i >= 0; i--) {
+9            fill(curr.begin(), curr.end(), 0);
+10            curr[m] = 1; 
+11            for(int j = m - 1; j >= 0; j--) {
+12                if(s[i] == t[j]) {
+13                    curr[j] += prev[j + 1]; 
+14                }
+15                curr[j] += prev[j];
+16            }
+17            swap(prev, curr);
+18        }
+19
+20        return prev[0];
+21    }
+22};
