880f9b2c4c008f0e787afc7b63ec28408c606a88
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-08-21 13:36:49 +0530

    File updated at 8/21/2026, 1:36:47 PM

--- 455-AssignCookies/455-AssignCookies.cpp
+++ 455-AssignCookies/455-AssignCookies.cpp
@@ -1,21 +1,20 @@
-// Last updated: 8/21/2026, 1:34:39 PM
+// Last updated: 8/21/2026, 1:36:47 PM
 1class Solution {
 2public:
 3    int findContentChildren(vector<int>& g, vector<int>& s) {
-4        sort(s.rbegin(), s.rend());
-5        sort(g.rbegin(), g.rend());
-6
-7        int idx1 = 0, idx2 = 0;
-8        int count = 0;
-9        while(idx1 < s.size()) {
-10            while(idx2 < g.size() && g[idx2] > s[idx1]) {
-11                idx2++;
-12            }
-13            if(idx2 == g.size()) return count;
-14            count += 1;
-15            idx2++;
-16            idx1++;
-17        }
-18        return count;
-19    }
-20};
+4        int n = g.size();
+5        int m = s.size();
+6        int l = 0, r = 0;
+7
+8        sort(g.begin(),g.end());
+9        sort(s.begin(),s.end());
+10
+11        while(l < m && r < n){
+12            if(g[r] <= s[l]){
+13                r++;
+14            }
+15            l++;
+16        }
+17        return r;
+18    }
+19};
