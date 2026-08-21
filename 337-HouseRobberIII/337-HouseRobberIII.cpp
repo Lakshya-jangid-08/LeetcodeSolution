dca58c1aaceef802fecb46683f3c2be3674805cb
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-08-22 00:25:23 +0530

    File updated at 8/22/2026, 12:25:23 AM

--- 337-HouseRobberIII/337-HouseRobberIII.cpp
+++ 337-HouseRobberIII/337-HouseRobberIII.cpp
@@ -1,30 +1,19 @@
-// Last updated: 8/22/2026, 12:21:51 AM
-1/**
-2 * Definition for a binary tree node.
-3 * struct TreeNode {
-4 *     int val;
-5 *     TreeNode *left;
-6 *     TreeNode *right;
-7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
-8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-10 * };
-11 */
-12class Solution {
-13    unordered_map<TreeNode*, unordered_map<int, int>> dp;
-14    int solve(TreeNode* root, int allow) {
-15        if(!root) return 0;
-16
-17        if(dp[root].count(allow)) return dp[root][allow];
-18
-19        int take = -1e5;
-20        if(allow)   
-21            take = root->val + solve(root->left, 0) + solve(root->right, 0);
-22        int skip = solve(root->left, 1) + solve(root->right, 1);
-23        return dp[root][allow] = max(take, skip);
-24    } 
-25public:
-26    int rob(TreeNode* root) {
-27        return solve(root, 1);
-28    }
-29};
+// Last updated: 8/22/2026, 12:25:23 AM
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    pair<long long, long long> dfs(TreeNode* node) {
+        if (!node) return {0, 0};
+        auto L = dfs(node->left);
+        auto R = dfs(node->right);
+        long long notRob = max(L.first, L.second) + max(R.first, R.second);
+        long long rob = node->val + L.first + R.first;
+        return {notRob, rob};
+    }
+    int rob(TreeNode* root) {
+        auto res = dfs(root);
+        return (int)max(res.first, res.second);
+    }
+};
