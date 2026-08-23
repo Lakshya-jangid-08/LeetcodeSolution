b99ac71f4320653e1ce89bd788334e78a887285b
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-08-23 23:39:09 +0530

    File updated at 8/23/2026, 11:39:07 PM

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.cpp
@@ -1,23 +1,23 @@
-// Last updated: 11/4/2026, 1:55:59 pm
+// Last updated: 8/23/2026, 11:39:07 PM
 1class Solution {
 2public:
 3    int characterReplacement(string s, int k) {
-4        int freq[26] = {0};
-5        
-6        int l = 0, maxFreq = 0, maxLen = 0;
-7
-8        for (int r = 0; r < s.size(); r++) {
-9            freq[s[r] - 'A']++;
-10            maxFreq = max(maxFreq, freq[s[r] - 'A']);
-11
-12            if(r - l + 1 - maxFreq > k) {
-13                freq[s[l] - 'A']--;
-14                l++;
-15            }
-16
-17            maxLen = max(maxLen, r - l + 1);
-18        }
-19
-20        return maxLen;
+4        int n = s.length();
+5        int len = 0;
+6        for(int i = 0; i < 26; i++) {
+7            int l = 0, r = 0;
+8            int count = 0;
+9            char allow = 'A' + i;
+10            while(r < n) {
+11                count += (s[r] != allow);
+12                while(count > k) {
+13                    count -= (s[l] != allow);
+14                    l++;
+15                }
+16                len = max(len, r - l + 1);
+17                r++;
+18            }
+19        }
+20        return len;
 21    }
 22};
