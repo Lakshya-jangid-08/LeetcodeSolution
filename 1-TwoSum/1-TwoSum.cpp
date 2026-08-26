8fb887c8fc7cc96c8d113302487b4265fee1b9ba
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-08-26 22:37:36 +0530

    File updated at 8/26/2026, 10:37:34 PM

--- 1-TwoSum/1-TwoSum.cpp
+++ 1-TwoSum/1-TwoSum.cpp
@@ -1,16 +1,16 @@
-// Last updated: 8/24/2026, 11:50:53 PM
+// Last updated: 8/26/2026, 10:37:34 PM
 1class Solution {
 2public:
 3    vector<int> twoSum(vector<int>& nums, int target)
 4    {
 5        unordered_map<int, int> map;
-6        int needed;
+6        int need;
 7        for (int i = 0; i < nums.size(); i++)
 8        {
-9            needed = target - nums[i];
-10            if (map.count(needed))
+9            need = target - nums[i];
+10            if (map.count(need))
 11            {
-12                return {map[needed], i};
+12                return {map[need], i};
 13            }
 14            map[nums[i]] = i;
 15        }
