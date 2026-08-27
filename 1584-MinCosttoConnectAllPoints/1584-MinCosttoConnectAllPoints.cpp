89eee9f50d3a21b218d450290e44768026ecbb25
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-08-27 23:56:37 +0530

    File updated at 8/27/2026, 11:56:36 PM

--- 1584-MinCosttoConnectAllPoints/1584-MinCosttoConnectAllPoints.cpp
+++ 1584-MinCosttoConnectAllPoints/1584-MinCosttoConnectAllPoints.cpp
@@ -1,4 +1,4 @@
-// Last updated: 8/27/2026, 11:37:42 PM
+// Last updated: 8/27/2026, 11:56:36 PM
 1class DisjointSet {
 2
 3    vector<int> parent;
@@ -37,32 +37,26 @@
 36public:
 37    int minCostConnectPoints(vector<vector<int>>& points) {
 38        vector<vector<int>> distances;
-39        unordered_map<string, int> mp;
-40        int idx = 0;
-41        for(int i = 0; i < points.size(); i++) {
-42            for(int j =  i + 1; j < points.size(); j++) {
-43                string u = to_string(points[i][0]) + " " + to_string(points[i][1]);
-44                string v = to_string(points[j][0]) + " " + to_string(points[j][1]);
-45
-46                if(!mp.count(u)) mp[u] = idx++;
-47                if(!mp.count(v)) mp[v] = idx++;
+39
+40        for(int i = 0; i < points.size(); i++) {
+41            for(int j =  i + 1; j < points.size(); j++) {
+42
+43                int d = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
+44                distances.push_back({i, j, d});
+45            }   
+46        }
+47
 48
-49                int d = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
-50                distances.push_back({mp[u], mp[v], d});
-51            }   
-52        }
-53
-54
-55        sort(distances.begin(), distances.end(), [](auto &d1, auto &d2) {
-56            return d1[2] < d2[2];
-57        });
-58        int n = distances.size();
-59        DisjointSet dsu(idx + 1);
-60        int cost = 0;
-61        for(int i = 0; i < n; i++) {
-62            int u = distances[i][0], v = distances[i][1], w = distances[i][2];
-63            if(dsu.join(u, v)) cost += w;
-64        }
-65        return cost;
-66    }
-67};
+49        sort(distances.begin(), distances.end(), [](auto &d1, auto &d2) {
+50            return d1[2] < d2[2];
+51        });
+52        int n = distances.size();
+53        DisjointSet dsu(points.size() + 1);
+54        int cost = 0;
+55        for(int i = 0; i < n; i++) {
+56            int u = distances[i][0], v = distances[i][1], w = distances[i][2];
+57            if(dsu.join(u, v)) cost += w;
+58        }
+59        return cost;
+60    }
+61};
