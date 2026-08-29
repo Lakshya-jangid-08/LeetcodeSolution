1a0053bf62eb221df9a898d3f0ae47280e9a51da
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-08-30 00:41:57 +0530

    File updated at 8/30/2026, 12:41:57 AM

--- 2948-MakeLexicographicallySmallestArraybySwappingElements/2948-MakeLexicographicallySmallestArraybySwappingElements.cpp
+++ 2948-MakeLexicographicallySmallestArraybySwappingElements/2948-MakeLexicographicallySmallestArraybySwappingElements.cpp
@@ -1,43 +1,73 @@
-// Last updated: 8/30/2026, 12:17:06 AM
+// Last updated: 8/30/2026, 12:41:57 AM
 1class Solution {
-2public:
-3    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
-4        vector<int> numsSorted(nums);
-5        sort(numsSorted.begin(), numsSorted.end());
-6
-7        int currGroup = 0;
-8        unordered_map<int, int> numToGroup;
-9        numToGroup.insert(pair<int, int>(numsSorted[0], currGroup));
-10
-11        unordered_map<int, list<int>> groupToList;
-12        groupToList.insert(
-13            pair<int, list<int>>(currGroup, list<int>(1, numsSorted[0])));
-14
-15        for (int i = 1; i < nums.size(); i++) {
-16            if (abs(numsSorted[i] - numsSorted[i - 1]) > limit) {
-17                // new group
-18                currGroup++;
-19            }
-20
-21            // assign current element to group
-22            numToGroup.insert(pair<int, int>(numsSorted[i], currGroup));
-23
-24            // add element to sorted group list
-25            if (groupToList.find(currGroup) == groupToList.end()) {
-26                groupToList[currGroup] = list<int>();
-27            }
-28            groupToList[currGroup].push_back(numsSorted[i]);
-29        }
-30
-31        // iterate through input and overwrite each element with the next
-32        // element in its corresponding group
-33        for (int i = 0; i < nums.size(); i++) {
-34            int num = nums[i];
-35            int group = numToGroup[num];
-36            nums[i] = *groupToList[group].begin();
-37            groupToList[group].pop_front();
-38        }
-39
-40        return nums;
-41    }
-42};
+2    vector<int> parent;
+3    vector<int> size;
+4
+5    int findPar(int x) {
+6        if(x == parent[x]) return x;
+7        return parent[x] = findPar(parent[x]);
+8    }
+9
+10    void join(int u, int v) {
+11        int pu = findPar(u);
+12        int pv = findPar(v);
+13
+14        if(pu == pv) return;
+15
+16        if(size[pu] < size[pv]) {
+17            parent[pu] = pv;
+18            size[pv] += size[pu];
+19        } else {
+20            parent[pv] = pu;
+21            size[pu] += size[pv];
+22        }
+23    }
+24
+25 public:
+26    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+27        int n = nums.size();
+28        parent = vector<int>(n, 0);
+29        size = vector<int>(n, 0);
+30        iota(parent.begin(), parent.end(), 0);
+31
+32        vector<pair<int, int>> temp;
+33        for(int i = 0; i < n; i++) {
+34            temp.push_back({nums[i], i});
+35        }
+36
+37        sort(temp.begin(), temp.end());
+38
+39        for(int i = 0; i < n - 1; i++) {
+40            auto &[v1, idx1] = temp[i];
+41            auto &[v2, idx2] = temp[i + 1];
+42            
+43            if(v2 - v1 <= limit) {
+44                join(idx1, idx2);
+45            }
+46        }
+47
+48        unordered_map<int , vector<int>> mp;
+49
+50        for(int i = n - 1; i >= 0; i--) {
+51            int par = findPar(i);
+52            mp[par].push_back(i);
+53        }
+54
+55        for(auto &[_, arr] : mp) {
+56            sort(arr.begin(), arr.end(), [&](auto &idx1, auto &idx2) {
+57                return nums[idx1] > nums[idx2];
+58            });
+59        }
+60
+61        vector<int> res(n);
+62        for(int i = 0; i < n; i++) {
+63            int par = findPar(i);
+64
+65            int idx = mp[par].back();
+66            mp[par].pop_back(); 
+67
+68            res[i] = nums[idx];
+69        }
+70        return res;
+71     }
+72};
