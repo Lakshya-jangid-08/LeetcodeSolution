142db83e3a415a789e20efb787c286d8bea4f7fe
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-09-03 11:16:25 +0530

    File updated at 9/3/2026, 11:16:24 AM

--- 3876-ConstructUniformParityArrayII/3876-ConstructUniformParityArrayII.cpp
+++ 3876-ConstructUniformParityArrayII/3876-ConstructUniformParityArrayII.cpp
@@ -1,46 +1,22 @@
-// Last updated: 9/3/2026, 11:09:26 AM
+// Last updated: 9/3/2026, 11:16:24 AM
 1class Solution {
 2public:
-3    bool uniformArray(vector<int>& nums) {
-4        //  even - odd => odd
-5        // odd - oddd => even
+3    bool uniformArray(vector<int>& nums1) {
+4        int mnv = *min_element(nums1.begin(), nums1.end());
+5        int minOdd = 1e8, minEven = 1e8;
 6
-7        int n = nums.size();
-8        int minodd = 1e8;
-9        int mineven = 1e8;
-10        // if all are even;
-11        for(int i = 0; i < n; i++) {
-12            if(nums[i] % 2 == 1)
-13                minodd = min(minodd, nums[i]);
-14            else 
-15                mineven = min(mineven, nums[i]);
-16        }
+7        for(auto &v : nums1) {
+8            if(v&1) minOdd = min(minOdd, v);
+9            else minEven = min(minEven, v);
+10        }
+11
+12        // even - odd => odd
+13        // odd - odd => even
+14
+15        int even = (mnv & 1) ? 0 : 1;
+16        if(even) return (minOdd == 1e8);
 17
-18        // if all oddd ban jaye
-19        bool ans = true;
-20        for(int i = 0; i < n; i++) {
-21            if(nums[i] % 2 == 1) {
-22                continue;
-23            } else {
-24                if(nums[i] - minodd >= 1) continue;
-25                else {
-26                    ans = false;
-27                    break;
-28                }
-29            }
-30        }
-31        bool res = true;
-32        for(int i = 0; i < n; i++) {
-33            if(nums[i] % 2 == 0) {
-34                continue;
-35            } else {
-36                if(nums[i] - minodd >= 1) continue;
-37                else {
-38                    res = false;
-39                    break;
-40                }
-41            }
-42        }
-43        return res | ans;
-44    }
-45};
+18        return true;
+19
+20    }
+21};
