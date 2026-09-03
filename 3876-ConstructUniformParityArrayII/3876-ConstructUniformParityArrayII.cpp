b23ad81d9981fb33b78fdea5a1290eeef2157f68
Lakshya Jangid <202351075@iiitvadodara.ac.in>
2026-09-03 11:19:56 +0530

    File updated at 9/3/2026, 11:19:56 AM

--- 3876-ConstructUniformParityArrayII/3876-ConstructUniformParityArrayII.cpp
+++ 3876-ConstructUniformParityArrayII/3876-ConstructUniformParityArrayII.cpp
@@ -1,22 +1,17 @@
-// Last updated: 9/3/2026, 11:16:24 AM
+// Last updated: 9/3/2026, 11:19:56 AM
 1class Solution {
 2public:
 3    bool uniformArray(vector<int>& nums1) {
-4        int mnv = *min_element(nums1.begin(), nums1.end());
-5        int minOdd = 1e8, minEven = 1e8;
-6
-7        for(auto &v : nums1) {
+4        int minOdd = 1e8, minEven = 1e8, mnv = 1e8;
+5
+6        for(auto &v : nums1) {
+7            mnv = min(mnv, v);
 8            if(v&1) minOdd = min(minOdd, v);
 9            else minEven = min(minEven, v);
 10        }
 11
 12        // even - odd => odd
 13        // odd - odd => even
-14
-15        int even = (mnv & 1) ? 0 : 1;
-16        if(even) return (minOdd == 1e8);
-17
-18        return true;
-19
-20    }
-21};
+14        return (mnv & 1) ? true : (minOdd == 1e8);
+15    }
+16};
